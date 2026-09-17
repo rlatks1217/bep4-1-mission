b22c7fa41c730773452e951852e248f8b50fe9b6
kim san <rlatks1217@gmail.com>
2026-09-17 18:30:38 +0900

    0006

--- src/main/java/com/back/boundedContext/post/entity/Post.java
+++ src/main/java/com/back/boundedContext/post/entity/Post.java
@@ -3,10 +3,13 @@ package com.back.boundedContext.post.entity;
 
 import com.back.boundedContext.member.entity.Member;
 import com.back.global.jpa.entity.BaseIdAndTime;
+import com.back.shared.post.dto.PostCommentDto;
+import com.back.shared.post.event.PostCommentCreatedEvent;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.util.ArrayList;
@@ -16,6 +19,7 @@ import static jakarta.persistence.CascadeType.PERSIST;
 import static jakarta.persistence.CascadeType.REMOVE;
 import static jakarta.persistence.FetchType.LAZY;
 
+@Getter
 @Entity
 @NoArgsConstructor
 public class Post extends BaseIdAndTime {
@@ -39,6 +43,7 @@ public class Post extends BaseIdAndTime {
         comments.add(postComment);
 
         author.increaseActivityScore(1);
+        publishEvent(new PostCommentCreatedEvent(new PostCommentDto(postComment)));
 
         return postComment;
     }
