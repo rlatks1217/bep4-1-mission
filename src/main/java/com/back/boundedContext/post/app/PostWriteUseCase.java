369960a45e7819dea9b2e90341c319b308f7e8bd
kim san <rlatks1217@gmail.com>
2026-09-17 18:56:58 +0900

    0009

--- src/main/java/com/back/boundedContext/post/app/PostWriteUseCase.java
+++ src/main/java/com/back/boundedContext/post/app/PostWriteUseCase.java
@@ -9,27 +9,21 @@ import com.back.shared.post.event.PostCreatedEvent;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
-
 @Service
 @RequiredArgsConstructor
-public class PostService {
+public class PostWriteUseCase {
     private final PostRepository postRepository;
     private final EventPublisher eventPublisher;
-
-    public long count() {
-        return postRepository.count();
-    }
 
     public Post write(Member author, String title, String content) {
         Post post = postRepository.save(new Post(author, title, content));
 
-        eventPublisher.publish(new PostCreatedEvent(new PostDto(post)));
+        eventPublisher.publish(
+                new PostCreatedEvent(
+                        new PostDto(post)
+                )
+        );
 
         return post;
-    }
-
-    public Optional<Post> findById(int id) {
-        return postRepository.findById(id);
     }
 }
