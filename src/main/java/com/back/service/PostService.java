06ad83910b5b3a5ae1ee825a7f0112ebcc01b8f0
kim san <rlatks1217@gmail.com>
2026-09-17 17:16:14 +0900

    0004

--- src/main/java/com/back/service/PostService.java
+++ src/main/java/com/back/service/PostService.java
@@ -1,4 +1,3 @@
-
 package com.back.service;
 
 import com.back.entity.Member;
@@ -20,6 +19,8 @@ public class PostService {
 
     public Post write(Member author, String title, String content) {
         Post post = new Post(author, title, content);
+
+        author.increaseActivityScore(3);
 
         return postRepository.save(post);
     }
