12d4fd2e0ae13a94d88953ccbb134f19e55a2ca0
kim san <rlatks1217@gmail.com>
2026-09-17 16:48:46 +0900

    0003

--- src/main/java/com/back/repository/PostRepository.java
+++ src/main/java/com/back/repository/PostRepository.java
@@ -1,4 +1,8 @@
+
 package com.back.repository;
 
-public class PostRepository {
+import com.back.entity.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostRepository extends JpaRepository<Post, Integer> {
 }
