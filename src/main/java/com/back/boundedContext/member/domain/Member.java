32318fe0d4ddf2165e437543a324c083f4d0aa6d
kim san <rlatks1217@gmail.com>
2026-09-18 12:14:52 +0900

    0016, 0017

--- src/main/java/com/back/boundedContext/member/domain/Member.java
+++ src/main/java/com/back/boundedContext/member/domain/Member.java
@@ -1,30 +1,23 @@
 package com.back.boundedContext.member.domain;
 
-import com.back.global.jpa.entity.BaseIdAndTime;
-import jakarta.persistence.Column;
+import com.back.shared.member.domain.SourceMember;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-@Getter
 @Entity
+@Table(name = "MEMBER_MEMBER")
+@Getter
 @NoArgsConstructor
-@Table(name="MEMBER_MEMBER")
-public class Member extends BaseIdAndTime {
-    @Column(unique = true)
-    private String username;
-    private String password;
-    private String nickname;
-    private int activityScore;
-
+public class Member extends SourceMember {
     public Member(String username, String password, String nickname) {
-        this.username = username;
-        this.password = password;
-        this.nickname = nickname;
+        super(username, password, nickname);
     }
 
     public int increaseActivityScore(int amount) {
-        return this.activityScore += amount;
+        setActivityScore(getActivityScore() + amount);
+
+        return getActivityScore();
     }
 }
--- src/main/java/com/back/boundedContext/post/domain/PostMember.java
+++ src/main/java/com/back/boundedContext/post/domain/PostMember.java
@@ -1,18 +1,18 @@
 package com.back.boundedContext.post.domain;
 
-import com.back.global.jpa.entity.BaseIdAndTime;
-import jakarta.persistence.Column;
+
+import com.back.shared.member.domain.ReplicaMember;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-@Getter
 @Entity
+@Table(name = "POST_MEMBER")
+@Getter
 @NoArgsConstructor
-public class PostMember extends BaseIdAndTime {
-    @Column(unique = true)
-    private String username;
-    private String password;
-    private String nickname;
-    private int activityScore;
+public class PostMember extends ReplicaMember {
+    public PostMember(String username, String password, String nickname) {
+        super(username, password, nickname);
+    }
 }
