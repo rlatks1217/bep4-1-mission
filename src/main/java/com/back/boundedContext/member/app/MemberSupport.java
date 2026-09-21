f03cd39aed1fc50b409ae945a71b9e9dbf94a141
kim san <rlatks1217@gmail.com>
2026-09-21 10:35:49 +0900

    0029

---
+++ src/main/java/com/back/boundedContext/member/app/MemberSupport.java
@@ -0,0 +1,28 @@
+package com.back.boundedContext.member.app;
+
+import com.back.boundedContext.member.domain.Member;
+import com.back.boundedContext.member.out.MemberRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Service
+@AllArgsConstructor
+public class MemberSupport {
+    private final MemberRepository memberRepository;
+
+    @Transactional(readOnly = true)
+    public long count() {
+        return memberRepository.count();
+    }
+
+    public Optional<Member> findByUsername(String username) {
+        return memberRepository.findByUsername(username);
+    }
+
+    public Optional<Member> findById(int id) {
+        return memberRepository.findById(id);
+    }
+}
